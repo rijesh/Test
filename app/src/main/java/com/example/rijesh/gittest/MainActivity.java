215fddd262cb72a5e4d10e64ffd313909c755a92
rijesh <kprije@gmail.com>
2015-06-20 21:33:26 -0700

    Update MainActivity.java
    Another Minor Change

--- app/src/main/java/com/example/rijesh/gittest/MainActivity.java
+++ app/src/main/java/com/example/rijesh/gittest/MainActivity.java
@@ -7,7 +7,8 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-// Minor change
+// Minor change 
+// Second Minor Change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
